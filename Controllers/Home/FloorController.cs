dc297720e55d85311b1707baf8deffcbe3a812a7
ljg <1132134484@qq.com>
2017-02-10 14:50:11 +0800

    First commit

---
+++ Controllers/Home/FloorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers.Home
+{
+    public class FloorController : Controller
+    {
+        //
+        // GET: /Floor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/Home/HeadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers.Home
+{
+    public class HeadController : Controller
+    {
+        //
+        // GET: /Head/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
